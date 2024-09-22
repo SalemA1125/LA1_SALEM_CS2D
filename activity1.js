8528bc80b232c6096609aba99861672a860a04da
SalemA1125 <andreavaldezsalem@gmail.com>
2024-09-22 16:27:29 +0800

    activity1.js

---
+++ activity1.js
@@ -0,0 +1,9 @@
+let name = "Andrea Valdez Salem";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "19";
+let facebookAcct = "Andrea Salem";
+
+console.log("Name:", name);
+console.log("What do you wanna become in your life:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Facebook Account:", facebookAcct);
