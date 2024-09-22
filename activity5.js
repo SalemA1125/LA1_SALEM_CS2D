5cfa9d5d7b18d76c70cb01a5800e09873993fbdc
SalemA1125 <andreavaldezsalem@gmail.com>
2024-09-22 16:59:17 +0800

    activity5.js

---
+++ activity5.js
@@ -0,0 +1,12 @@
+let favActorFirstName = "Coco";
+let favActorLastName = "Martin";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = `My favorite Actor is ${uppercase}. He is a great actor and known for his action movies`;
+message += " His best show is Silicon Valley.";
+
+console.log("Favorite Actor's First Name:", favActorFirstName);
+console.log("Favorite Actor's Last Name:", favActorLastName);
+console.log("Full Name:", fullName);
+console.log("Uppercase Name:", uppercase);
+console.log("Message:", message);
