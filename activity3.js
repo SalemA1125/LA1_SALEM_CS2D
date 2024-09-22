485d86cb51c77ca34b74294782d51d1b1b9abe49
SalemA1125 <andreavaldezsalem@gmail.com>
2024-09-22 16:36:00 +0800

    activity3.js

---
+++ activity3.js
@@ -0,0 +1,9 @@
+let isJSProgrammingLanguage = true;
+let isJSHard = false;
+let favNumb = 04;
+let undefinedResult = favNumb + undefined;
+
+console.log("Is JavaScript a programming language:", isJSProgrammingLanguage);
+console.log("Is JavaScript hard:", isJSHard);
+console.log("Favorite Number:", favNumb);
+console.log("Result of adding undefined to a number:", undefinedResult);
