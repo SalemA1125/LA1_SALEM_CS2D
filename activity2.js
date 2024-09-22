fd80cd19af6bf94a8a7f6667830391f843002671
SalemA1125 <andreavaldezsalem@gmail.com>
2024-09-22 16:32:44 +0800

    activity2.js

---
+++ activity2.js
@@ -0,0 +1,15 @@
+let firstFavNum = 12;
+let secondFavNum = 25;
+let sum = firstFavNum + secondFavNum;
+let difference = firstFavNum - secondFavNum;
+let product = firstFavNum * secondFavNum;
+let quotient = firstFavNum / secondFavNum;
+let remainder = firstFavNum % secondFavNum;
+let power = Math.pow(firstFavNum, secondFavNum);
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
+console.log("Power:", power);
