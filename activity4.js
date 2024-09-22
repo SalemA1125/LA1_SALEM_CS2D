84ed08c65ba27fcb95385371596aee7d9a8ac1dc
SalemA1125 <andreavaldezsalem@gmail.com>
2024-09-22 16:42:01 +0800

    activity4.js

---
+++ activity4.js
@@ -0,0 +1,19 @@
+let firstFavNumb = 25;
+let secondFavNumb = 11;
+let isGreater = firstFavNumb > secondFavNumb;
+let isLess = firstFavNumb < secondFavNumb;
+let isGreaterOrEqual = firstFavNumb >= secondFavNumb;
+let isLessOrEqual = firstFavNumb <= secondFavNumb;
+let isStrictlyEqual = firstFavNumb === secondFavNumb;
+let isLooselyEqual = firstFavNumb == secondFavNumb;
+let isStrictlyNotEqual = firstFavNumb !== secondFavNumb;
+let isLooselyNotEqual = firstFavNumb != secondFavNumb;
+
+console.log("Is firstFavNumb greater than secondFavNumb:", isGreater);
+console.log("Is firstFavNumb less than secondFavNumb:", isLess);
+console.log("Is firstFavNumb greater than or equal to secondFavNumb:", isGreaterOrEqual);
+console.log("Is firstFavNumb less than or equal to secondFavNumb:", isLessOrEqual);
+console.log("Is firstFavNumb strictly equal to secondFavNumb:", isStrictlyEqual);
+console.log("Is firstFavNumb loosely equal to secondFavNumb:", isLooselyEqual);
+console.log("Is firstFavNumb strictly not equal to secondFavNumb:", isStrictlyNotEqual);
+console.log("Is firstFavNumb loosely not equal to secondFavNumb:", isLooselyNotEqual);
